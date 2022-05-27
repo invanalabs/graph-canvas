a580272314ae494212825ece8f2458b2bf1071d8
Ravi Raja Merugu <rrmerugu@gmail.com>
2022-05-27 08:10:02 +0530

    removed json2csv convertion code; code cleanups and structure refactors - wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 // import ExampleView from "./views/example-view";
-import SimpleExampleView from "./views/simple-example-view"
+import ExampleView from "./views/simple-example-view"
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <React.StrictMode>
-        <SimpleExampleView/>
+        <ExampleView/>
     </React.StrictMode>
 );
 
