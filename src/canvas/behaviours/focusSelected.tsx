824a3d5d8d03b026839cf3482b61aac67568122b
Ravi Raja Merugu <rrmerugu@gmail.com>
2022-09-01 14:25:57 +0530

    focusSelcted behaviour added, but this will block the node:click event on ClickSelect behaviour

---
+++ src/canvas/behaviours/focusSelected.tsx
@@ -0,0 +1,27 @@
+import React, {useContext, useEffect} from 'react';
+import Graphin, {IG6GraphEvent, Utils, GraphinData, GraphinContext} from '@antv/graphin';
+import {INode, NodeConfig} from '@antv/g6';
+import {highlightNodeById} from "@antv/graphin/es/apis/element";
+
+// https://graphin.antv.vision/en-US/graphin/register/behaviors/
+
+const FocusSelectedNodes = () => {
+    const {graph, apis} = useContext(GraphinContext);
+
+    useEffect(() => {
+        // if previously selected, use this to set.
+        // apis.focusNodeById('node-1');
+
+        const handleClick = (evt: IG6GraphEvent) => {
+            const node = evt.item as INode;
+            const model = node.getModel() as NodeConfig;
+            apis.focusNodeById(model.id);
+        };
+        graph.on('node:click', handleClick);
+        return () => {
+            graph.off('node:click', handleClick);
+        };
+    }, []);
+    return null;
+};
+export default FocusSelectedNodes
