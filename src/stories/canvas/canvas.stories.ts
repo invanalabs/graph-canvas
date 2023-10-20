94470cbcd7849f6232f1fc930c24e31cfd152612
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-10-21 03:27:51 +0530

    styling cleanups

--- src/stories/canvas/canvas.stories.ts
+++ src/stories/canvas/canvas.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import FlowCanvas from '../../canvas/canvas';
 import { initialNodes as erInitialNodes, initialEdges as erInitialEdges  } from "../../example-datasets/raw/er-mock-data";
 import { initialNodes, initialEdges } from '../../example-datasets/raw/simple-hello-world';
+import { initialNodes as allNodesInitialNodes, initialEdges as allEdgesInitialEdges  } from "../../example-datasets/raw/all-nodes";
 
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -23,7 +24,12 @@ const meta = {
   export default meta;
 type Story = StoryObj<typeof meta>;
 
-
+export const AllDataCanvas: Story = {
+  args: {
+      initialNodes:  allNodesInitialNodes,
+      initialEdges: allEdgesInitialEdges,
+  },
+};
 console.log("[=======initialNodes" , initialNodes)
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const SimpleStoryCanvas: Story = {
@@ -39,3 +45,5 @@ export const EntityRelationCanvas: Story = {
       initialEdges: erInitialEdges,
   },
 };
+
+
