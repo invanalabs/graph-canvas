4120f1eea93a561e70e0e5661c689fadc01bfc05
Ravi Raja Merugu <rrmerugu@gmail.com>
2023-10-22 18:53:50 +0530

    NoLayoutCanvas added

---
+++ src/flow-canvas/layouts/noLayout.ts
@@ -0,0 +1,8 @@
+import { CanvasEdge, CanvasNode } from "../core/types";
+import { FlowInstanceType } from "../interactions/interactions";
+
+
+export const defaultLayoutChange = (nodes: CanvasNode[], edges: CanvasEdge[], flowInstance: FlowInstanceType, direction: string) => {
+    console.log("=====defaultLayoutChange", nodes, edges)
+    return {layoutedNodes: nodes, layoutedEdges: edges}
+  }
---
+++ src/stories/canvas/noLayoutCanvas.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import BaseFlowCanvas from '../../flow-canvas/canvasTypes/BaseFlowCanvas';
+import {nodes, edges} from "../../example-datasets/noLayout/noLayoutData"
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'Canvas/NoLayoutFlowCanvas',
+    component: BaseFlowCanvas,
+    parameters: {
+      // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+      layout: 'fullscreen',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+    //   backgroundColor: { control: 'color' },
+    },
+  } satisfies Meta<typeof BaseFlowCanvas>;
+  
+  export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const NoLayoutFlowCanvas: Story = {
+  args: {
+      initialNodes:  nodes,
+      initialEdges: edges,
+  },
+}; 
