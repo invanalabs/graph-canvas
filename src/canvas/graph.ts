c8e5f11cd5bba4b66c7b65f6a21d8603beff87c4
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-02-28 02:53:40 +0530

    eventbar added to canvas story to see the events triggerd

--- src/canvas/graph.ts
+++ src/canvas/graph.ts
@@ -187,6 +187,14 @@ class GraphCanvas {
             // resolution: 2, //window.devicePixelRatio
         });
         // viewport.moveCenter(0, 0);
+
+     // _viewport.ensureVisible(0, 0, 5000, 5000, true)
+
+      // _viewport.clampZoom({ minScale: 0.2, maxScale: 2.5 })
+      // _viewport.clampZoom({ minWidth: 1000, minHeight: 1000, maxWidth: 5000, maxHeight: 5000 })
+
+      // _viewport.drag({ pressDrag: false })
+
         return viewport
     }
 
--- src/stories/canvas/canvas.ts
+++ src/stories/canvas/canvas.ts
@@ -48,9 +48,19 @@ export const createPage = () => {
     const canvasDiv = document.createElement('canvas');
     canvasDiv.style.width = "100vw"
     canvasDiv.style.height = "100vh"
-    canvasDiv.style.border = "10px solid red"
+    // canvasDiv.style.border = "10px solid red"
 
     html.appendChild(canvasDiv)
+
+    // create event list 
+    const eventbar = document.createElement('div');
+    eventbar.classList.add("eventbar");
+    eventbar.style.top = "50px";
+    eventbar.style.left = "0px";
+    eventbar.style.height = "100vh";
+    eventbar.style.position = "absolute"
+    html.appendChild(eventbar);
+
 
 
     // create toolbar
@@ -72,10 +82,23 @@ export const createPage = () => {
     fitViewButton.innerHTML = "fitView"
     toolbar.appendChild(fitViewButton);
 
-
-
-    
     html.appendChild(toolbar)
+
+
+    const showEvent = (eventType: string)=> {
+
+        const eventDiv = document.createElement('div')
+        eventDiv.innerHTML = `fired - ${eventType}`;
+        eventDiv.style.borderBottom = "2px solid #999"
+        eventDiv.style.padding = "10px "
+        eventDiv.style.marginBottom = "5px"
+        eventDiv.style.background = "#000"
+        eventDiv.style.opacity = "0.5"
+        eventDiv.style.width = "200px";
+        eventDiv.style.color = "white"
+
+        eventbar.prepend(eventDiv)
+    }
 
 
     document.addEventListener("DOMContentLoaded", function(event) {
@@ -87,6 +110,31 @@ export const createPage = () => {
         zoomInButton.addEventListener('click', () => canvas.zoomIn());
         zoomOutButton.addEventListener('click', () => canvas.zoomOut());
         fitViewButton.addEventListener('click', () => canvas.fitView());
+
+
+
+
+        canvas.viewport.on('clicked', () => showEvent('clicked'));
+        canvas.viewport.on('drag-start', () => showEvent('drag-start'));
+        canvas.viewport.on('drag-end', () => showEvent('drag-end'));
+        canvas.viewport.on('pinch-start', () => showEvent('pinch-start'));
+        canvas.viewport.on('pinch-end', () => showEvent('pinch-end'));
+        canvas.viewport.on('bounce-start-x', () => showEvent('bounce-start-x'));
+        canvas.viewport.on('bounce-end-x', () => showEvent('bounce-end-x'));
+        canvas.viewport.on('bounce-start-y', () => showEvent('bounce-start-y'));
+        canvas.viewport.on('bounce-end-y', () => showEvent('bounce-end-y'));
+        canvas.viewport.on('snap-start', () => showEvent('snap-start'));
+        canvas.viewport.on('snap-end', () => showEvent('snap-end'));
+        canvas.viewport.on('snap-zoom-start', () => showEvent('snap-zoom-start'));
+        canvas.viewport.on('snap-zoom-end', () => showEvent('snap-zoom-end'));
+        canvas.viewport.on('mouse-edges-start', () => showEvent('mouse-edges-start'));
+        canvas.viewport.on('mouse-edges-end', () => showEvent('mouse-edges-end'));
+        canvas.viewport.on('moved-end', () => showEvent('moved-end'));
+        canvas.viewport.on('zoomed-end', () => showEvent('zoomed-end'));
+        canvas.viewport.on("pointerover", () => showEvent('pointerover'));
+        canvas.viewport.on("pointerout", () => showEvent('pointerout'));
+
+
     }, false);
 
     // const section = `<section class="storybook-page"></section>`;
