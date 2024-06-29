1ce2b60c394cc8c36705ee8e5ab027c35a93c12f
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-06-29 09:08:45 +0530

    refactored graphics to shapes

---
+++ src/renderer/shapes/base.ts
@@ -0,0 +1,332 @@
+
+import * as PIXI from 'pixi.js';
+import { CanvasLink, CanvasNode } from '../../store';
+import { ILinkStateTypes, INodeStateTypes } from '../types';
+import { ArtBoard } from '../../artBoard';
+import { OnLinkGfxEventData, OnNodeGfxEventData } from '../../store/events/types';
+
+
+// abstract class ShapeStateManagerAbstract {
+
+//     // determines whether this is 
+//     abstract stateName: INodeStateTypes | ILinkStateTypes
+
+//     abstract setState(stateName: INodeStateTypes| ILinkStateTypes): void
+//     // :default
+//     abstract setDefault(): void
+//     // :hover
+//     abstract settHovered(): void
+//     // :highlighted
+//     abstract setHighlighted(): void
+//     // :inactive
+//     abstract setInactive():void
+//     // :hidden
+//     abstract setHidden():void
+// }
+
+
+abstract class ShapeAbstractBase {
+
+    abstract readonly originalData: CanvasLink | CanvasNode;
+    /* latest state of data */
+    abstract data: CanvasLink | CanvasNode;
+    // abstract state : INodeStateTypes | ILinkStateTypes
+
+    /* graphics */
+    abstract containerGfx: PIXI.Graphics;
+    declare labelGfx: PIXI.Graphics | undefined
+    declare shapeGfx: PIXI.Graphics | undefined;
+    declare borderGfx: PIXI.Graphics | undefined
+
+
+
+    /* this will  */
+    abstract processData(data: CanvasLink | CanvasNode): CanvasLink | CanvasNode;
+
+    // drawing
+    abstract drawShape(): PIXI.Graphics | undefined;
+    abstract drawLabel(): PIXI.Graphics | undefined;
+    abstract draw(drawShape: boolean, drawLabel: boolean): void;
+    abstract redraw(): void;
+
+
+    // delete 
+    abstract clear(): void;
+    abstract destroy(): void;
+
+    // events and triggers
+    abstract setupInteractionTriggers(): void;
+    abstract removeInteractionTriggers(): void
+
+    abstract setState(state: INodeStateTypes | ILinkStateTypes, setNeighborsToo: boolean, event?: PIXI.FederatedPointerEvent): void
+    // :default
+    abstract triggerDefault(event?: PIXI.FederatedPointerEvent): void
+    // :hover
+    abstract triggerHovered(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerHoveredOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+    abstract triggerUnHovered(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnHoveredOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+    // :highlighted
+    abstract triggerHighlighted(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerHighlightedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+    abstract triggerUnHighlighted(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnHighlightedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+
+    // :inactive
+    abstract triggerInactive(event?: PIXI.FederatedPointerEvent): void
+    // :hidden
+    abstract triggerHidden(event?: PIXI.FederatedPointerEvent): void
+
+
+    // layers - front, data, map 
+    abstract moveToFrontLayer(): void
+    abstract moveToDataLayer(): void
+    // abstract moveToMapLayer(): void
+
+
+}
+
+
+
+
+
+export abstract class ShapeAbstract extends ShapeAbstractBase {
+
+    originalData: CanvasNode | CanvasLink
+    artBoard: ArtBoard
+    containerGfx: PIXI.Graphics;
+
+    // for state based graphics :hovered and :highlighted
+    declare shapeHoveredGfx: PIXI.Graphics;
+    declare shapeHighlightedGfx: PIXI.Graphics;
+
+
+    constructor(data: CanvasNode | CanvasLink, artBoard: ArtBoard) {
+        super()
+        this.originalData = data
+        this.artBoard = artBoard;
+        this.containerGfx = new PIXI.Graphics()
+        this.containerGfx.interactive = true
+        // Set the cursor style to 'pointer' when hovering over the sprite
+        // this.containerGfx.cursor = 'pointer';
+        // this.containerGfx.setAn
+        // in v8; { isRenderGroup:true} // this containers transform is now handled on the GPU!
+        // Make the containerGfx interactive...
+        // this.containerGfx.cursor = 'pointer';
+        // this.containerGfx.eventMode = 'static';
+    }
+
+    // Function to recursively set interactive and cursor properties on all children
+    setInteractiveRecursive(container: PIXI.Graphics) {
+        container.interactive = true;
+        container.cursor = 'pointer';
+        container.children.forEach((child) => {
+            this.setInteractiveRecursive(child as PIXI.Graphics);
+        });
+    }
+
+    clear = () => {
+        console.log("ShapeAbstract.clear triggered")
+        this.containerGfx.removeChildren();
+    }
+
+    removeInteractionTriggers() {
+        console.debug("===removeInteractionTriggers triggered on link", this.containerGfx)
+        // Remove all listeners
+        this.containerGfx.removeAllListeners();
+    }
+
+    setState(stateName: INodeStateTypes, setNeighborsToo: boolean = false, event?: PIXI.FederatedPointerEvent) {
+        console.log("==setState", this.data.id, stateName, setNeighborsToo)
+
+        if (this.data.state === stateName)
+            return
+
+
+        this.artBoard.canvas.dataStore.setState(this.data, stateName, setNeighborsToo, event)
+        // this.state = stateName
+    }
+
+
+
+
+    drawDebugBorder(x: number, y: number) {
+        // Calculate the bounding box
+        // console.log("===drawDebugBorder", this.data.id)
+        // // const borderGfx = this.artBoard.viewport.getChildByName(NodeContainerChildNames.debugBorder)
+        // if (this.borderGfx){
+        //     // const position = this.containerGfx.getBounds();
+        //     // const position = this.containerGfx.getLocalBounds()
+        //     this.borderGfx.position.set(x,y)
+        //     // this.borderGfx.position.set(
+        //     //     this.containerGfx.position.x /2,
+        //     //     this.containerGfx.position.y /2
+        //     // )
+        // }else{
+        //     const bounds = this.containerGfx.getBounds();
+        //     // const position = event ? event.data.getLocalPosition(): {x: bounds.x, y: bounds.y}
+        //     // Draw a debug box around the bounding box
+        //     this.borderGfx = new PIXI.Graphics();
+        //     this.borderGfx.name = NodeContainerChildNames.debugBorder
+        //     this.borderGfx.lineStyle(2, 0x00ff00);
+        //     this.borderGfx.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+        //     this.borderGfx.endFill()
+        //     this.artBoard.viewport.addChild(this.borderGfx);
+        // }
+    }
+
+}
+
+
+export abstract class NodeShapeAbstract extends ShapeAbstract {
+
+    declare readonly originalData: CanvasNode;
+    /* latest state of data */
+    declare data: CanvasNode;
+
+    // // determines whether this is 
+    // declare state: INodeStateTypes
+
+    /* this will  */
+    abstract processData(data: CanvasNode): CanvasNode;
+
+    // set position of node 
+    abstract setPosition(x: number, y: number): void;
+
+    abstract triggerHoveredOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnHoveredOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+    abstract triggerHighlightedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnHighlightedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+
+    abstract triggerSelected(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnSelected(event?: PIXI.FederatedPointerEvent): void
+
+    // abstract triggerSelectedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+    // abstract triggerUnSelectedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+
+
+    abstract onPointerIn(event : OnNodeGfxEventData): void
+    abstract onPointerOut(event : OnNodeGfxEventData): void
+    abstract onClicked(event : OnNodeGfxEventData): void
+    abstract onUnClicked(event : OnNodeGfxEventData): void
+    abstract onMoved(event : OnNodeGfxEventData): void
+
+
+    abstract onDragStart(event?: PIXI.FederatedPointerEvent): void
+    // abstract onDragMove(event?: PIXI.FederatedPointerEvent,  newPoint: PIXI.Point): void
+    abstract onDragEnd(event?: PIXI.FederatedPointerEvent): void
+
+
+
+    applyStateUpdate(setNeighborsToo: boolean = false, event?: PIXI.FederatedPointerEvent) {
+        const stateName = this.data.state
+        if (stateName === ":default") {
+            this.triggerUnHovered(event);
+            this.triggerUnHighlighted(event);
+            if (this.triggerUnSelected) {
+                this.triggerUnSelected(event)
+            }
+            this.triggerDefault(event);
+
+            if (setNeighborsToo) {
+                this.triggerUnHoveredOnNeighbors(event)
+                this.triggerUnHighlightedOnNeighbors(event)
+            }
+        }
+        else if (stateName === ":hovered") {
+            this.triggerHovered(event)
+            if (setNeighborsToo) {
+                this.triggerHoveredOnNeighbors(event)
+            }
+        }
+        else if (stateName === ":selected") {
+            this.triggerSelected(event)
+        }
+
+        else if (stateName === ":highlighted") {
+            this.triggerHighlighted(event)
+            if (setNeighborsToo) {
+                this.triggerHighlightedOnNeighbors(event)
+            }
+        }
+        else if (stateName === ":inactive") {
+            this.triggerInactive(event)
+        }
+        else if (stateName === ":hidden") {
+            this.triggerHidden(event)
+        }
+    }
+
+
+
+}
+
+
+export abstract class LinkShapeAbstract extends ShapeAbstract {
+
+    declare readonly originalData: CanvasLink;
+    /* latest state of data */
+    declare data: CanvasLink;
+
+    // determines whether this is 
+    // declare state: ILinkStateTypes
+
+    /* this will  */
+    abstract processData(data: CanvasLink): CanvasLink;
+
+    abstract triggerHoveredOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnHoveredOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+    abstract triggerHighlightedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+    abstract triggerUnHighlightedOnNeighbors(event?: PIXI.FederatedPointerEvent): void
+
+
+
+    abstract onPointerIn(event : OnLinkGfxEventData): void
+    abstract onPointerOut(event : OnLinkGfxEventData): void
+    abstract onClicked(event : OnLinkGfxEventData): void
+    abstract onUnClicked(event : OnLinkGfxEventData): void
+    abstract onMoved(event : OnLinkGfxEventData): void
+
+
+
+    applyStateUpdate(setNeighborsToo: boolean = false, event?: PIXI.FederatedPointerEvent) {
+        const stateName = this.data.state
+        if (stateName === ":default") {
+            this.triggerUnHovered(event);
+            this.triggerUnHighlighted(event);
+            this.triggerDefault(event);
+            if (setNeighborsToo) {
+                this.triggerUnHoveredOnNeighbors(event)
+                this.triggerUnHighlightedOnNeighbors(event)
+            }
+        }
+        else if (stateName === ":hovered") {
+            this.triggerHovered(event)
+            if (setNeighborsToo) {
+                this.triggerHoveredOnNeighbors(event)
+            }
+        }
+
+        else if (stateName === ":highlighted") {
+            this.triggerHighlighted(event)
+            if (setNeighborsToo) {
+                this.triggerHighlightedOnNeighbors(event)
+            }
+        }
+        else if (stateName === ":inactive") {
+            this.triggerInactive(event)
+        }
+        else if (stateName === ":hidden") {
+            this.triggerHidden(event)
+        }
+    }
+
+}
---
+++ src/renderer/shapes/links/straight/straight.ts
@@ -0,0 +1,78 @@
+import * as PIXI from 'pixi.js';
+import { LinkShapeBase } from '../base';
+import { getAngle, getContactPointFromCircle, getContactPointOnCircle, getLinkLabelPosition } from '../utils';
+
+
+class StraightLink extends LinkShapeBase{
+
+
+    // labelGfx: PIXI.Graphics; 
+    // shapeGfx: PIXI.Graphics;
+
+    // constructor(data: ICanvasLink, canvas: GraphCanvas) {
+    //     super(data, canvas)
+
+    //     this.data = this.processData(data)
+    //     const gfxs = this.draw(true, true);
+    //     this.labelGfx = gfxs.labelGfx; // 
+    //     this.shapeGfx = gfxs.shapeGfx;
+    //     // setup intractions
+    //     this.setupInteractions()
+    // }
+
+    //@ts-ignore
+    // point: PIXI.Point;
+    //@ts-ignore
+    curveType: ILinkShapeStyles = 'straight'
+
+    calcLabelPosition = (labelGfx: PIXI.Graphics, shapeGfx: PIXI.Graphics) => {
+        const labelPosition = getLinkLabelPosition(this.data.source, this.data.target, this.curveType)
+        labelGfx.angle = this.calcLabelAngle(shapeGfx)
+        labelGfx.position.set(labelPosition.x, labelPosition.y);
+
+    }
+
+    calcLabelAngle =(shapeGfx: PIXI.Graphics) => {
+        let angle = getAngle(this.data.source, this.data.target);
+        if (angle > 90 || angle < -90) {
+          angle = angle + 180;
+        }
+        return angle
+    }
+
+    // calcArrowAngle = (arrow: PIXI.Graphics, startPoint: PIXI.Point, endPoint: PIXI.Point,  points: number[], ) => {
+    //     arrow.rotation = Math.atan2(
+    //         startPoint.y - endPoint.y,
+    //         startPoint.x - endPoint.x
+    //     );
+    //     arrow.position.set(endPoint.x, endPoint.y);
+    // }
+
+    calcStartAndEndPoints = () => {
+            // line color and thickness
+            // console.log("calcStartAndEndPoints", JSON.stringify(this.data))
+        console.debug("====calcStartAndEndPoints", this.data, this)
+        const arrowPadding = 3; 
+        const endPoint = getContactPointOnCircle(
+            this.data.source,
+            this.data.target,
+            arrowPadding
+        );
+        const startPoint = getContactPointFromCircle(
+            this.data.source,
+            this.data.target,
+            arrowPadding
+        );
+        return {startPoint, endPoint}
+    
+    }
+
+    // drawPath = (shapeLine: PIXI.Graphics, startPoint: PIXI.Point, endPoint: PIXI.Point) => {
+    //     shapeLine.moveTo(startPoint.x, startPoint.y);
+    //     shapeLine.lineTo(endPoint.x, endPoint.y);
+
+    // }
+}
+
+
+export default StraightLink
