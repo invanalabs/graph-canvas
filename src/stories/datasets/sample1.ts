a2b5a04a8a72a466a8b0bb1a13c438a7ad70eef3
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-07-25 13:22:06 +0530

    dataset cleanups

--- src/stories/datasets/sample1.ts
+++ src/stories/datasets/sample1.ts
@@ -6,20 +6,22 @@ const exampleNodes: ICanvasNode[] = [
   { id: '3', group: 'Person', label: 'Person-3', x: 100, y: 450 },
   { id: '4', group: 'Person', label: 'Person-4', x: 450, y: 450 },
   { id: '5', group: 'Project', label: 'Project', x: (100+450)/2, y: (100+450)/2 },
+  { id: '6', group: 'Person', label: 'Person-6', x: 750, y: 450 },
 
 ];
 
 const exampleLinks: ICanvasLink[] = [
-  { id: '1-2', group: 'follows', label: 'default-1-2', source: '1', target: '2' },
+  { id: '1-2', group: 'follows', label: 'follows', source: '1', target: '2' },
   // { id: '1-2.1', group: 'follows', label: 'default-1-2.1', source: '1', target: '2' },
-  { id: '2-3', group: 'follows', label: 'default-2-3', source: '2', target: '3' },
-  { id: '2-4', group: 'follows', label: 'default-2-4', source: '2', target: '4' },
-  { id: '3-4', group: 'follows', label: 'default-3-4', source: '3', target: '4' },
+  // { id: '2-3', group: 'follows', label: 'follows', source: '2', target: '3' },
+  { id: '2-4', group: 'follows', label: 'follows', source: '2', target: '4' },
+  { id: '3-4', group: 'follows', label: 'follows', source: '3', target: '4' },
+  { id: '4-6', group: 'follows', label: 'follows', source: '4', target: '6' },
 
 
   { id: '1-5', group: 'contributing_to', label: 'contributing_to', source: '1', target: '5' },
   { id: '2-5', group: 'contributing_to', label: 'contributing_to', source: '2', target: '5' },
-  { id: '3-5', group: 'contributing_to', label: 'contributing_to', source: '3', target: '5' },
+  // { id: '3-5', group: 'contributing_to', label: 'contributing_to', source: '3', target: '5' },
   { id: '4-5', group: 'contributing_to', label: 'contributing_to', source: '4', target: '5' },
 ];
 
