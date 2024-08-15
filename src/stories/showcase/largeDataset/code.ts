c07b04564680b1c3057807b2b8b98d24e9e748a6
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-08-15 12:48:35 +0530

    code cleanup chores

---
+++ src/stories/showcase/largeDataset/code.ts
@@ -0,0 +1,20 @@
+import { GraphCanvas } from "../../../canvas";
+import largeData from "../../example-datasets/large-data/largeData.json";
+
+
+export default () => {
+
+  const canvasDiv = document.getElementById("graphCanvas") as HTMLCanvasElement
+
+  const canvas = new GraphCanvas({
+    viewElement: canvasDiv,
+    debugMode: true
+  });
+
+
+  canvas.artBoard.init().then(() => {
+    canvas.dataStore.add(largeData.nodes, largeData.links)
+    canvas.artBoard.camera.fitView();
+  })
+
+}
