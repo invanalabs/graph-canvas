ec67c2addced9b9ffd3160a67dc5805d4c126e75
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-08-19 11:40:24 +0530

    label style cleanups

--- src/stories/features/nodes/styling/codes/label-styling.ts
+++ src/stories/features/nodes/styling/codes/label-styling.ts
@@ -17,7 +17,7 @@ export default () => {
   // https://www.toptal.com/designers/htmlarrows/symbols/
   const nodes: ICanvasNode[] = [
     {id: '1', group: 'DemoNode', label: 'simple label',  x: -100, y: -100,  },
-    {id: '3', group: 'DemoNode', label: '<h3>Heading here</h3><p>This is formated <em>italic</em> <strong>strong</strong> text.</p>',  x: -100 , y: -400, },
+    {id: '3', group: 'DemoNode', label: '<h3>Heading here</h3><p>This is formated <em>italic</em> <strong>strong</strong> text.</p>',  x: -100 , y: -300, },
     // {id: '4', group: 'DemoNode', label: 'image:jpg',   x: -100, y: -300, },
     // {id: '5', group: 'DemoNode', label: 'icon:Unicode', x: 100, y: -100, icon: '\u2729' }, //'\uf007'}
     // {id: '6', group: 'DemoNode', label: 'icon:HTMLCode', x: 100, y: -200, icon: '&percnt;' },//'\uf007'}
