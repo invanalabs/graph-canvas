8e260a0781bb0dcabf898358292827210ba49f82
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-11-17 02:38:34 +0530

    symmetric polygon shape added

--- src/stories/shapes/symmetricalPolygon/index.ts
+++ src/stories/shapes/symmetricalPolygon/index.ts
@@ -11,38 +11,30 @@ export default () => {
   renderer.init().then(() => {
     console.log("Renderer initialized");
 
-    // with default styling
-    const shape = new SymmetricalPolygon();
+    // draw triangle
+    const shape = new SymmetricalPolygon({ sidesCount: 3 });
     shape.x = 100
     shape.y = 100;
     renderer.addGfx(shape);
 
-    // with border
-    const shapeWithBorder = new SymmetricalPolygon({ border: { width: 10 } });
-    shapeWithBorder.x = 500
-    shapeWithBorder.y = 100;
-    renderer.addGfx(shapeWithBorder);
-
-
 
     // draw square
     const square = new SymmetricalPolygon({ sidesCount: 4 });
-    square.x = 100
-    square.y = 300;
+    square.x = 300
+    square.y = 100;
     renderer.addGfx(square);
-
 
 
     // draw pentagon
     const pentagon = new SymmetricalPolygon({ sidesCount: 5 });
-    pentagon.x = 300
-    pentagon.y = 100;
+    pentagon.x = 100
+    pentagon.y = 300;
     renderer.addGfx(pentagon);
 
     // draw hexagon
     const hexagon = new SymmetricalPolygon({ sidesCount: 6 });
-    pentagon.x = 100
-    pentagon.y = 600;
+    pentagon.x = 300
+    pentagon.y = 300;
     renderer.addGfx(hexagon);
 
   });
