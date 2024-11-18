a9c7826d1595ddc417588727f822681edfc93dea
Ravi Raja Merugu <rrmerugu@gmail.com>
2024-11-18 17:32:22 +0530

    adjusting the aspect ratio of the image

--- src/stories/shapes/symmetricalPolygon/index.ts
+++ src/stories/shapes/symmetricalPolygon/index.ts
@@ -12,33 +12,56 @@ export default () => {
     console.log("Renderer initialized");
 
     // draw triangle
-    const shape = new SymmetricalPolygon({ sidesCount: 3 });
+    const shape = new SymmetricalPolygon({
+      sidesCount: 3,
+      fill: { alpha: 0.5, imageUrl: "https://invana.io/public/img/vendor-logos/janusgraph.png" }
+    });
     shape.setPosition(100, 100)
     renderer.addGfx(shape);
 
     // draw square
-    const square = new SymmetricalPolygon({ sidesCount: 4 });
+    const square = new SymmetricalPolygon({
+      sidesCount: 4,
+      fill: { alpha: 0.5, imageUrl: "https://invana.io/public/img/vendor-logos/janusgraph.png" }
+
+    });
     square.setPosition(300, 100)
     renderer.addGfx(square);
 
     // draw pentagon
-    const pentagon = new SymmetricalPolygon({ sidesCount: 5 });
+    const pentagon = new SymmetricalPolygon({
+      sidesCount: 5,
+      fill: { alpha: 0.5, imageUrl: "https://invana.io/public/img/vendor-logos/janusgraph.png" }
+
+    });
     pentagon.setPosition(100, 300)
     renderer.addGfx(pentagon);
 
     // draw hexagon
-    const hexagon = new SymmetricalPolygon({ sidesCount: 6 });
+    const hexagon = new SymmetricalPolygon({
+      sidesCount: 6,
+      fill: { alpha: 0.5, imageUrl: "https://invana.io/public/img/vendor-logos/janusgraph.png" }
+
+    });
     hexagon.setPosition(300, 300)
     renderer.addGfx(hexagon);
 
     // draw hexagon
-    const heptagon = new SymmetricalPolygon({ sidesCount: 7 });
+    const heptagon = new SymmetricalPolygon({
+      sidesCount: 7,
+      fill: { alpha: 0.5, imageUrl: "https://invana.io/public/img/vendor-logos/janusgraph.png" }
+
+    });
     heptagon.setPosition(100, 500)
     renderer.addGfx(heptagon);
 
 
     // draw octagon
-    const octagon = new SymmetricalPolygon({ sidesCount: 8 });
+    const octagon = new SymmetricalPolygon({
+      sidesCount: 8,
+      fill: { alpha: 0.5, imageUrl: "https://invana.io/public/img/vendor-logos/janusgraph.png" }
+
+    });
     octagon.setPosition(300, 500)
     renderer.addGfx(octagon);
 
